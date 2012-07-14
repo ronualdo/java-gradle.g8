e84b5990cff027d4d7fde947ce96b22e2e92b74a
ronualdo <raxmac@gmail.com>
2012-07-14 09:51:30 -0300

    Implementation to base classes

--- src/main/g8/src/main/java/$organization__packaged$/$name__lower$/Application.java
+++ src/main/g8/src/main/java/$organization__packaged$/$name__lower$/Application.java
@@ -1,1 +1,7 @@
 package $organization$.$name;format="lower"$;
+
+public class Application {
+  public static void main(String[] args) {
+    System.out.println("Funcionando");
+  }
+}
--- src/main/g8/src/test/java/$organization__packaged$/$name__lower$/ApplicationTest.java
+++ src/main/g8/src/test/java/$organization__packaged$/$name__lower$/ApplicationTest.java
@@ -2,3 +2,7 @@ package $organization$.$name;format="lower"$;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+public class ApplicationTest {
+  
+}
